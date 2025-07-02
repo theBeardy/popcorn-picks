dbb2e65e0d6e8677f1f0c375de5b9087f8d23739
theBeardy <keatoncbutler@gmail.com>
2025-07-02 09:00:24 -0400

    Further Tailwind Styling, fixed Login Form and Film Details views

--- users/views.py
+++ users/views.py
@@ -1,8 +1,17 @@
+from django import forms
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
 from film_ratings.models import Movie, Review
+
+class TailwindAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'border bg-gray-100 border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500'
+            })
 
 def register_view(request):
     if request.method == "POST":
@@ -16,7 +25,7 @@ def register_view(request):
 
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+        form = TailwindAuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
             if 'next' in request.POST:
@@ -24,7 +33,7 @@ def login_view(request):
             else:
                 return redirect("users:dashboard")
     else: 
-        form = AuthenticationForm()
+        form = TailwindAuthenticationForm()
     return render(request, "users/login_view.html", { 'form' : form })
 
 def logout_view(request):
