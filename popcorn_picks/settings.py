ebc2676b7722f8f2b4718a4da7dd363faffac409
theBeardy <keatoncbutler@gmail.com>
2025-07-16 20:16:46 -0400

    fixed staticfules_dirs

--- popcorn_picks/settings.py
+++ popcorn_picks/settings.py
@@ -149,4 +149,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme')]
---
+++ staticfiles/static_src/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+    "postcss-simple-vars": {},
+    "postcss-nested": {}
+  },
+}
